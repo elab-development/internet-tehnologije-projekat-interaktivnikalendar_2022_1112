1f219ecb5a3054d57155cf72661d69a06a8a3f52
cvijeticc <marko.andrija@yahoo.com>
2024-07-30 12:01:53 +0200

    definicija kontrolera vezanih za autentifikaciju i dogadjaje

--- laraveldomaci/app/Http/Controllers/DogadjajController.php
+++ laraveldomaci/app/Http/Controllers/DogadjajController.php
@@ -4,62 +4,145 @@ namespace App\Http\Controllers;
 
 use App\Models\Dogadjaj;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\DogadjajResource;
 
 class DogadjajController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    // Prikaz svih dogadjaja
     public function index()
     {
-        //
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze prikazati sve dogadjaje!'], 403);
+        }
+        $dogadjaji = Dogadjaj::all();
+        return DogadjajResource::collection($dogadjaji);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // Prikazivanje pojedinacnog dogadjaja
+    public function show($id)
     {
-        //
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze prikazati dogadjaj na osnovu ID-ija!'], 403);
+        }
+        $dogadjaj = Dogadjaj::findOrFail($id);
+        return new DogadjajResource($dogadjaj);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    // Unos dogadjaja
     public function store(Request $request)
     {
-        //
+    $user_id = Auth::user()->id;
+    $validator = Validator::make($request->all(), [
+            'naziv' => 'required',
+            'opis' => 'required',
+            'datum' => 'required',
+            'lokacija_id' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
+
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze kreirati dogadjaje!'], 403);
+        }
+
+        $dogadjaj = new Dogadjaj();
+        $dogadjaj->naziv = $request->naziv;
+        $dogadjaj->opis = $request->opis;
+        $dogadjaj->datum = $request->datum;
+        $dogadjaj->lokacija_id = $request->lokacija_id;
+        $dogadjaj->user_id = $user_id;
+
+
+        $dogadjaj->save();
+
+        return response()->json(['Uspešno kreiran novi dogadjaj!', new DogadjajResource($dogadjaj)]);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Dogadjaj $dogadjaj)
+    // Azuriranje informacija o dogadjaju
+    public function update(Request $request, $id)
     {
-        //
+        $user_id = Auth::user()->id;
+        $validator = Validator::make($request->all(), [
+            'naziv' => 'required',
+            'opis' => 'required',
+            'datum' => 'required',
+        
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+            return response()->json($errors);
+        }
+
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze menjati dogadjaje!'], 403);
+        }
+
+        $dogadjaj_user_id = Dogadjaj::where('id', $id)->value('user_id');
+
+        if($user_id != $dogadjaj_user_id){
+            return response()->json(['error' => 'Vi niste korisnik koji je kreirao ovaj dogadjaj!'], 403);
+        }
+
+        $dogadjaj = Dogadjaj::findOrFail($id);
+
+        $dogadjaj->naziv = $request->naziv;
+        $dogadjaj->opis = $request->opis;
+        $dogadjaj->datum = $request->datum;
+        
+
+        $dogadjaj->save();
+
+        return response()->json(['Uspešno ažuriran dogadjaj!', new DogadjajResource($dogadjaj)]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Dogadjaj $dogadjaj)
+    // Azuriranje samo opisa u dogadjaju
+    public function updateOpis(Request $request, $id)
     {
-        //
+        $user_id = Auth::user()->id;
+        $request->validate([
+            'opis' => 'required'
+        ]);
+
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze menjati dogadjaje!'], 403);
+        }
+
+        $dogadjaj_user_id = Dogadjaj::where('id', $id)->value('user_id');
+
+    if($user_id != $dogadjaj_user_id){
+        return response()->json(['error' => 'Vi niste korisnik koji je kreirao ovaj dogadjaj!'], 403);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Dogadjaj $dogadjaj)
-    {
-        //
+
+        $dogadjaj = Dogadjaj::findOrFail($id);
+    
+        $dogadjaj->update(['opis' => $request->input('opis')]);
+    
+        return response()->json(['message' => 'Opis datog dogadjaja je uspesno izmenjen!', new DogadjajResource($dogadjaj)]);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Dogadjaj $dogadjaj)
+    // Brisanje dogadjaja
+    public function destroy($id)
     {
-        //
+        $user_id = Auth::user()->id;
+        $dogadjaj_user_id = Dogadjaj::where('id', $id)->value('user_id');
+
+        if(Auth::user()->isAdmin){
+            return response()->json(['error' => 'Admin ne moze menjati dogadjaje!'], 403);
+        }
+
+        if($user_id != $dogadjaj_user_id){
+            return response()->json(['error' => 'Vi niste korisnik koji je kreirao ovaj dogadjaj!'], 403);
+        }
+
+        $dogadjaj = Dogadjaj::findOrFail($id);
+        $dogadjaj->delete();
+        return response()->json('Uspešno obrisan dogadjaj!');
     }
 }
