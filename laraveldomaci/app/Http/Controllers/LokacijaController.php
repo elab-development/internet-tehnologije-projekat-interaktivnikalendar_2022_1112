5db88dfc61c2f7054440a223867d6b3e4f04cc84
cvijeticc <marko.andrija@yahoo.com>
2024-07-30 11:50:26 +0200

    kreiranje kontrolera

---
+++ laraveldomaci/app/Http/Controllers/LokacijaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lokacija;
+use Illuminate\Http\Request;
+
+class LokacijaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lokacija $lokacija)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lokacija $lokacija)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Lokacija $lokacija)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lokacija $lokacija)
+    {
+        //
+    }
+}
