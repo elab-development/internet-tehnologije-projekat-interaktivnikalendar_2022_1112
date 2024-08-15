74d9301e88255074c16c15aee83ca5520fab5f70
cvijeticc <marko.andrija@yahoo.com>
2024-08-15 04:48:35 +0200

    dopuna naznacivanja trenutnog datuma

--- reactdomaci/src/components/CalendarApp.jsx
+++ reactdomaci/src/components/CalendarApp.jsx
@@ -45,7 +45,7 @@ const CalendarApp = () => {
       <div className="calendar">
         <h1 className="heading"> Calendar </h1>
         <div className="navigate-date">
-          <h2 className="month">{monthsOfYear[currentMonth]}</h2>
+          <h2 className="month">{monthsOfYear[currentMonth]},</h2>
           <h2 className="year">{currentYear}</h2>
           <div className="buttons">
             <i className="bx bx-chevron-left" onClick={prevMonth}></i>
@@ -62,7 +62,18 @@ const CalendarApp = () => {
             <span key={`empty-${index}`} />
           ))}
           {[...Array(daysInMonth).keys()].map((day) => (
-            <span key="{day+1}">{day + 1}</span>
+            <span
+              key={day + 1}
+              className={
+                day + 1 === currentDate.getDate() &&
+                currentMonth === currentDate.getMonth() &&
+                currentYear === currentDate.getFullYear()
+                  ? "current-day"
+                  : ""
+              }
+            >
+              {day + 1}
+            </span>
           ))}
         </div>
       </div>
