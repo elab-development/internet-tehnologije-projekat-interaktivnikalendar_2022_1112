16aadb86a2f3ac12b5fb998581a85b65c392011a
AleksandarJocovic2 <bob560633@gmail.com>
2024-08-15 04:35:05 +0200

    Dodavanje dinamike popunjavanja kalendarskih dana u mesecu

--- reactdomaci/src/components/CalendarApp.jsx
+++ reactdomaci/src/components/CalendarApp.jsx
@@ -1,4 +1,44 @@
+import { useState } from "react";
+
 const CalendarApp = () => {
+  const daysOfWeek = ["Pon", "Uto", "Sre", "Cet", "Pet", "Sub", "Ned"];
+  const monthsOfYear = [
+    "Januar",
+    "Februar",
+    "Mart",
+    "April",
+    "Maj",
+    "Jun",
+    "Jul",
+    "Avgust",
+    "Septembar",
+    "Octobar",
+    "Novembar",
+    "Decembar",
+  ];
+  const currentDate = new Date();
+
+  const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+  const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); //koliko mesec ima dana
+  const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay(); //kojim danom se pocinje mesec
+
+  //iteriranje za prelazak na prethodni mesec
+  const prevMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 0 ? 11 : prevMonth - 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 0 ? prevYear - 1 : prevYear
+    );
+  };
+  //iteriranje za prelazak na sledeci mesec
+  const nextMonth = () => {
+    setCurrentMonth((prevMonth) => (prevMonth === 11 ? 0 : prevMonth + 1));
+    setCurrentYear((prevYear) =>
+      currentMonth === 11 ? prevYear + 1 : prevYear
+    );
+  };
+
   return (
     <div className="calendar-app">
       <div className="calendar">
@@ -7,50 +47,22 @@ const CalendarApp = () => {
           <h2 className="month">May,</h2>
           <h2 className="year">2024</h2>
           <div className="buttons">
-            <i className="bx bx-chevron-left"></i>
-            <i className="bx bx-chevron-right"></i>
+            <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+            <i className="bx bx-chevron-right" onClick={nextMonth}></i>
           </div>
         </div>
         <div className="weekdays">
-          <span>Mon</span>
-          <span>Tue</span>
-          <span>Wed</span>
-          <span>Thu</span>
-          <span>Fri</span>
-          <span>Sat</span>
-          <span>Sun</span>
+          {daysOfWeek.map((day) => (
+            <span key={day}>{day}</span>
+          ))}
         </div>
         <div className="days">
-          <span>1</span>
-          <span>2</span>
-          <span>3</span>
-          <span>4</span>
-          <span>5</span>
-          <span>6</span>
-          <span>7</span>
-          <span>8</span>
-          <span>9</span>
-          <span>10</span>
-          <span>11</span>
-          <span>12</span>
-          <span>13</span>
-          <span>14</span>
-          <span>15</span>
-          <span>16</span>
-          <span className="current-day">17</span>
-          <span>18</span>
-          <span>19</span>
-          <span>20</span>
-          <span>21</span>
-          <span>22</span>
-          <span>23</span>
-          <span>24</span>
-          <span>25</span>
-          <span>26</span>
-          <span>27</span>
-          <span>28</span>
-          <span>29</span>
-          <span>30</span>
+          {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+            <span key={`empty-${index}`} />
+          ))}
+          {[...Array(daysInMonth).keys()].map((day) => (
+            <span key="{day+1}">{day + 1}</span>
+          ))}
         </div>
       </div>
       <div className="events">
