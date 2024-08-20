fd2285f30e4eb846a23a832cf668a2267c738cd8
AleksandarJocovic2 <bob560633@gmail.com>
2024-08-20 02:36:07 +0200

    Implementacija korisnickog profila i prikazivanja liste korisnika

---
+++ seminarskirad/frontreact/src/components/UserProfile.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const UserProfile = () => {
+  const { userId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/users/${userId}`
+        );
+        setUser(response.data.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUser();
+  }, [userId]);
+
+  if (!user) {
+    return <div>Učitavanje...</div>;
+  }
+
+  return (
+    <div className="user-profile-container">
+      <h2>Profil korisnika</h2>
+      <p>ID: {user.id}</p>
+      <p>Ime: {user.name}</p>
+      <p>Email: {user.email}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
